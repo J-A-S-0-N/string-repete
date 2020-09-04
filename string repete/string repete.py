08dba8fe78a0d9922a2163a99fbc9a4b28cb82c1
J-4-S-0-N <62867830+J-A-S-0-N@users.noreply.github.com>
2020-09-04 23:41:18 +0800

    Add files via upload

---
+++ string repete/string repete.py
@@ -0,0 +1,11 @@
+def answer(a,b):
+    c = ""
+    answer_lis = []
+    for i in range(a):
+        answer_lis.append(b)
+    # print(answer_lis)
+    answer_lis = c.join(answer_lis)
+    print(answer_lis)
+    return answer_lis
+
+print(answer(2, "a"))
